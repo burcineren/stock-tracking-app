122cdfcd4b8de4ec698f9e67d4a5adc485682647
burcineren <burcin.ern969@gmail.com>
2024-03-18 13:46:22 +0300

    feat: table stock filter by multiple stock

--- src/app/pages/stock-tracking/stock-tracking.component.ts
+++ src/app/pages/stock-tracking/stock-tracking.component.ts
@@ -118,6 +118,7 @@ export class StockTrackingComponent implements OnInit {
   dataSource = new MatTableDataSource<StockElement>();
   displayedColumns: string[] = ['symbol', 'date', 'openPrice'];
   stocks = ['IBM', 'AAPL', 'AMZN', 'GOOG', 'MSFT'];
+  selectedStocks: string[] = []; 
   height = 400;
   type = "msline";
   dataFormat = "json";
@@ -126,29 +127,27 @@ export class StockTrackingComponent implements OnInit {
     start: new FormControl<string | null>(null),
     end: new FormControl<string | null>(null),
   });
-
   constructor(private stockService: StockService) { }
-
   ngOnInit(): void {
   }
-
   filteredDataSource = new MatTableDataSource<StockElement>();
 
   filterData() {
     const startDate = this.formatDate(this.range.value.start);
     const endDate = this.formatDate(this.range.value.end);
-    if (startDate && endDate) {
-      this.stockService.getDailyTimeSeries(this.stocks, startDate, endDate).subscribe((data) => {
+    const selectedStocks = this.selectedStocks; 
+    if (startDate && endDate && selectedStocks.length > 0) {
+      this.stockService.getDailyTimeSeries(selectedStocks, startDate, endDate).subscribe((data) => {
         const stockElements: StockElement[] = [];
-
+  
         const symbols: string[] = Object.keys(data);
-
+  
         for (const symbol of symbols) {
           if (data[symbol] && data[symbol]['Time Series (Daily)'] && data[symbol]['Meta Data']) {
             const metaData = data[symbol]['Meta Data'];
             const timeSeries = data[symbol]['Time Series (Daily)'];
             const symbolName = metaData['2. Symbol'];
-
+  
             for (const [date, values] of Object.entries(timeSeries)) {
               const openPrice = parseFloat(values['1. open']);
               stockElements.push({ date, openPrice, symbol: symbolName });
@@ -157,32 +156,23 @@ export class StockTrackingComponent implements OnInit {
             console.error(`Time Series (Daily) data for stock ${symbol} is missing or undefined`);
           }
         }
-
         stockElements.sort((a, b) => a.symbol.localeCompare(b.symbol));
-
         this.filteredDataSource.data = stockElements;
         this.applyFilter();
       });
-
+  
     } else {
-      console.log('Please select a date range');
+      console.log('Please select a date range and at least one stock');
     }
   }
-
-
-
-
-
   formatDate(date: string | null): string | null {
     if (!date) {
       return null;
     }
-
     const dateObj = new Date(date);
-    const day = String(dateObj.getDate()).padStart(2, '0'); // Zero-pad the day
-    const month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Zero-pad the month
+    const day = String(dateObj.getDate()).padStart(2, '0'); 
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
     console.log("first:::", day);
-    // Reorder month and day for DD-MM-YYYY format
     return `${day}-${month}-${dateObj.getFullYear()}`;
   }
   applyFilter() {
